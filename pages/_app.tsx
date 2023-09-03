62516b95074fca94b325c16e5f864cb9bbe572f7
Sharath M <nayak.sharathm@gmail.com>
2023-09-04 00:44:57 +0530

    Add Id attribute for Script Tag.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,12 +13,12 @@ import Transition from "../components/Transition";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Script
+    <Script id="gtm-script"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="gtm-lazy-script" strategy="lazyOnload">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
